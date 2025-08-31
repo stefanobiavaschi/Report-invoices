447a0a7a2f61f024ed588a84cef29fbb6cf6458d
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2025-08-31 13:31:17 +0200

    update-2507

--- 01.main.py
+++ 01.main.py
@@ -9,10 +9,11 @@ input_dirs = [
     "data/raw/25_04_01-25_05_30",
     "data/raw/25_05_31-25_06_25",
     "data/raw/25_06_26-25_07_12",
+    "data/raw/25_07_13-25_08_07",
 ]
 
 
 xmlproc.process_xml(input_dirs)
 
 # Select date in the format "yymmdd" for create excel files
-postprocess(data_string="250713")
+postprocess(data_string="250809")
